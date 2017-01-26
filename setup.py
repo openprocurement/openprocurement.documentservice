d95f762ee5f75303250ceb3548e2c4be7a02acaf
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2017-01-26 15:30:19 +0200

    prepare 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
-version = '1.0'
+version = '1.0.1'
 
 requires = [
     'chaussette',
